00df9de6f9e4cc58556055dc3ae77dc01911e782
Igor Knop <igorknop@gmail.com>
2023-10-02 22:24:39 -0300

    add played pile

--- cards.js
+++ cards.js
@@ -1,115 +1,161 @@
-import {turnClockwise, turnAntiClockwise, increaseSpeed, decreaseSpeed, increaseSize} from './snake.js';
+import {
+  turnClockwise,
+  turnAntiClockwise,
+  increaseSpeed,
+  decreaseSpeed,
+  increaseSize,
+} from "./snake.js";
 
 const cards = [
-    {efeito: "turn-left", symbol: "↻"},
-    {efeito: "turn-right", symbol:"↺"},
-    {efeito: "increase-speed", symbol: "⇡"}, 
-    {efeito: "decrease-speed", symbol: "⭭"},
-    {efeito: "block-player", symbol: "⭙"},
-    {efeito: "take-two", symbol: "🃟"},
-    {efeito: "increase-size", symbol: "+"},
-    {efeito: "decrease-size", symbol: "-"},
-    {efeito: "play-again", symbol: "🍀"}
-]
+  { efeito: "turn-left", symbol: "↻" },
+  { efeito: "turn-right", symbol: "↺" },
+  { efeito: "increase-speed", symbol: "⇡" },
+  { efeito: "decrease-speed", symbol: "⭭" },
+  { efeito: "block-player", symbol: "⭙" },
+  { efeito: "take-two", symbol: "🃟" },
+  { efeito: "increase-size", symbol: "+" },
+  { efeito: "decrease-size", symbol: "-" },
+  { efeito: "play-again", symbol: "🍀" },
+];
 const deck = [
-    {...cards[0]},
-    {...cards[1]},
-    {...cards[2]},
-    {...cards[3]},
-    {...cards[4]},
-    {...cards[5]},
-    {...cards[6]},
-    {...cards[7]},
-    {...cards[8]}
+  { ...cards[0] },
+  { ...cards[1] },
+  { ...cards[2] },
+  { ...cards[3] },
+  { ...cards[4] },
+  { ...cards[5] },
+  { ...cards[6] },
+  { ...cards[7] },
+  { ...cards[8] },
 ];
 const hand = [];
 
-const discardPile = [];
+const discard = [];
+const played = [];
 
 const obstacles = [
-    {x: 1, y: 1},
-    {x: 3, y: 6},
-    {x: 5, y: 2},
-    {x: 7, y: 5},
-    {x: 9, y: 3},
-]
+  { x: 1, y: 1 },
+  { x: 3, y: 6 },
+  { x: 5, y: 2 },
+  { x: 7, y: 5 },
+  { x: 9, y: 3 },
+];
 
 const bonus = [
-    {x: 1, y: 7, type: "increase-speed"},
-    {x: 3, y: 3, type: "increase-size"},
-    {x: 5, y: 5, type: "play-again"},
-    {x: 7, y: 1, type: "take-two"},
-]
+  { x: 1, y: 7, type: "increase-speed" },
+  { x: 3, y: 3, type: "increase-size" },
+  { x: 5, y: 5, type: "play-again" },
+  { x: 7, y: 1, type: "take-two" },
+];
 
-function getCards(){
-    return cards;
+function getCards() {
+  return cards;
 }
 
-function getDeck(){
-    return deck;
+function getDeck() {
+  return deck;
 }
 
-function getHand(){
-    return hand;
+function getHand() {
+  return hand;
 }
 
-function getObstacles(){
-    return obstacles;
+function getObstacles() {
+  return obstacles;
 }
 
-function getBonus(){
-    return bonus;
+function getBonus() {
+  return bonus;
 }
 
-function playCard(card)
-{
-    switch(card.efeito)
-    {
-        case "turn-left": turnClockwise();  break;
-        case "turn-right": turnAntiClockwise(); break;
-        case "increase-speed": increaseSpeed(); break;
-        case "decrease-speed": decreaseSpeed(); break;
-        case "block-player": break;
-        case "take-two": break;
-        case "increase-size": increaseSize(); break;
-        case "decrease-size": break;
-        case "play-again": break;
-    }
-    hand.forEach(c=>discardPile.push(c));
-    hand.splice(0, hand.length);
+function getDiscard() {
+  return discard;
 }
 
-function drawHand(){
-    if(deck.length>0){
-        shuffle(deck);
-        hand.push(deck.pop());
-    }else{
-        deck.push(...discardPile);
-        discardPile.splice(0, discardPile.length);
-        hand.push(deck.pop());
-    }
+function getPlayed() {
+  return played;
+}
+
+function playCard(card) {
+  console.log("played: ", card);
+  switch (card.efeito) {
+    case "turn-left":
+      turnClockwise();
+      break;
+    case "turn-right":
+      turnAntiClockwise();
+      break;
+    case "increase-speed":
+      increaseSpeed();
+      break;
+    case "decrease-speed":
+      decreaseSpeed();
+      break;
+    case "block-player":
+      break;
+    case "take-two":
+      break;
+    case "increase-size":
+      increaseSize();
+      break;
+    case "decrease-size":
+      break;
+    case "play-again":
+      break;
+  }
+  const idx = hand.findIndex((c) => c.efeito === card.efeito);
+  played.push(hand.splice(idx, 1)[0]);
+  hand.forEach((c) => discard.push(c));
+  hand.splice(0, hand.length);
+  console.log(played, discard);
+}
+
+function drawHand() {
+  if (deck.length > 0) {
+    hand.push(deck.pop());
+  } else {
+    deck.push(...discard);
+    deck.push(...played);
+    played.splice(0, played.length);
+    discard.splice(0, discard.length);
+    shuffle(deck);
+    hand.push(deck.pop());
+  }
 }
 
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex > 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
 
-function getRandom(num){
-    return Math.floor(Math.random()*num);
+function getRandom(num) {
+  return Math.floor(Math.random() * num);
 }
 
-export { getHand, getObstacles, getBonus, playCard, getCards, getDeck, getRandom, drawHand};
+export {
+  getHand,
+  getObstacles,
+  getBonus,
+  playCard,
+  getCards,
+  getDeck,
+  getRandom,
+  drawHand,
+  getDiscard,
+  getPlayed,
+};
--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 import { moveSnake, getSnake } from "./snake.js";
-import { getBonus, getObstacles, getHand, getDeck, getCards, playCard, getRandom, drawHand} from "./cards.js";
+import {
+  getBonus,
+  getObstacles,
+  getHand,
+  getDeck,
+  getCards,
+  playCard,
+  getRandom,
+  drawHand,
+  getDiscard,
+  getPlayed,
+} from "./cards.js";
 
 const snakeHeadDiv = document.querySelector("#head");
 let snakeBodyDiv = document.querySelectorAll(".snake.body");
@@ -7,20 +18,22 @@ const snakeTailDiv = document.querySelector(".snake.tail");
 
 let arrastado = null;
 let rodada = 1;
-const eDeck = document.querySelector(".deck");
+const eDeck = document.querySelector("#deck");
 
 let cont = getDeck().length;
-const eCont = document.createElement("span");
+const eCont = document.createElement("div");
 eCont.classList.add(".cont");
 eCont.textContent = cont;
 eDeck.appendChild(eCont);
 
-const eDiscardPile = document.querySelector(".discardPile");
-const eHand = document.querySelector(".deck-jogador");
+const ePlayedPile = document.querySelector("#played");
+const eDiscardPile = document.querySelector("#discard");
+const eHand = document.querySelector("#hand");
 const eObstacles = document.querySelector(".obstacles");
 
-eDiscardPile.addEventListener("dragover", dragOver);
-eDiscardPile.addEventListener("drop", receiveCard);
+ePlayedPile.addEventListener("dragover", dragOver);
+ePlayedPile.addEventListener("drop", receiveCard);
+eDiscardPile.addEventListener("click", reshuffleCards);
 
 const eCards = createCards();
 
@@ -34,21 +47,22 @@ function updateSnake() {
   snakeHeadDiv.style.gridColumn = snake.head.x;
   snakeHeadDiv.style.gridRow = snake.head.y;
   snakeHeadDiv.dataset.hit = snake.head.hit;
-  
+
   //console.log(snakeHeadDiv.style.gridColumn);
   //console.log(snakeHeadDiv.style.gridRow);
-  
+
   //update body in grid
-  if(snakeBodyDiv.length < Object.keys(snake.body).length){ //problema no length
+  if (snakeBodyDiv.length < Object.keys(snake.body).length) {
+    //problema no length
     const newBodyDiv = document.createElement("div");
-    newBodyDiv.setAttribute("style", "gridColumn = 0, gridRow = 0")
+    newBodyDiv.setAttribute("style", "gridColumn = 0, gridRow = 0");
     newBodyDiv.classList.add(".snake.body");
-    newBodyDiv.style.gridColumn = snake.body[snakeBodyDiv.length-1].x;
-    newBodyDiv.style.gridRow = snake.body[snakeBodyDiv.length-1].y;
+    newBodyDiv.style.gridColumn = snake.body[snakeBodyDiv.length - 1].x;
+    newBodyDiv.style.gridRow = snake.body[snakeBodyDiv.length - 1].y;
   }
   snakeBodyDiv = document.querySelectorAll(".snake.body");
 
-  for(let i=0; i<snakeBodyDiv.length; i++){
+  for (let i = 0; i < snakeBodyDiv.length; i++) {
     snakeBodyDiv[i].style.gridColumn = snake.body[i].x;
     snakeBodyDiv[i].style.gridRow = snake.body[i].y;
   }
@@ -68,89 +82,116 @@ buttonAnda.addEventListener('click', function(){
   updateSnake();
 });
 */
-function updateCards(){
+function updateCards() {
   drawHand();
   drawHand();
   drawHand();
-  
+
   eHand.replaceChildren();
-  for(let i=0; i<getHand().length; i++){
-    const idx = getCards().findIndex(c=>c.symbol === getHand()[i].symbol);
+  for (let i = 0; i < getHand().length; i++) {
+    const idx = getCards().findIndex((c) => c.symbol === getHand()[i].symbol);
     eHand.appendChild(eCards[idx]);
   }
   const deck = getDeck();
   eCont.textContent = deck.length;
 }
 
-function createCards(){
+function createCards() {
   const cards = getCards();
-  const eCards = []; 
-  for(let i=0; i<cards.length; i++){
-    const eCard = document.createElement("span");
-    eCard.classList.add("card");
-    eCard.setAttribute("draggable", true);
-    eCard.addEventListener("dragstart", dragCard);
-    eCard.dataset.efeito = cards[i].efeito;
-    eCard.dataset.symbol = cards[i].symbol;
-    eCard.dataset.id = i;
-
-    const eSymbol = document.createElement("span");
-    eSymbol.textContent = cards[i].symbol;
-    console.log(cards[i].symbol);
-    
-    eCard.appendChild(eSymbol);
+  const eCards = [];
+  for (let i = 0; i < cards.length; i++) {
+    const eCard = createCard(cards[i], i);
     eCards.push(eCard);
   }
   return eCards;
 }
 
-function createObstacles(){
+function createObstacles() {
   const obstacles = getObstacles();
   const eObstacles = [];
-  for(let i=0; i<obstacles.length; i++){
-    const eObstacle = document.createElement("span");
+  for (let i = 0; i < obstacles.length; i++) {
+    const eObstacle = document.createElement("div");
     eObstacle.classList.add(".obstacles");
-    
+
     eObstacles.push(eObstacle);
   }
   return eObstacles;
 }
 
-function placeObstacles(){
+function placeObstacles() {
   const obstacles = getObstacles();
   const idx = getRandom(getObstacles().length);
   eObstacles[idx].style.gridColumn = obstacles[idx].x;
   eObstacles[idx].style.gridRow = obstacles[idx].y;
 }
 
-function dragCard(event){
+function dragCard(event) {
   arrastado = event.target;
 }
 
-function dragOver(event){
-    event.preventDefault();
+function dragOver(event) {
+  event.preventDefault();
 }
 
-function receiveCard(event){
-  if(arrastado == null) {return};
+function updateDiscard() {
+  const discard = getDiscard();
+  eDiscardPile.innerHTML = "";
+  discard.forEach((c) => eDiscardPile.appendChild(createCard(c)));
+}
+function updatePlayed() {
+  const played = getPlayed();
+  ePlayedPile.innerHTML = "";
+  played.forEach((c) => ePlayedPile.appendChild(createCard(c)));
+}
+function receiveCard(event) {
+  if (arrastado == null) {
+    return;
+  }
   //if(event.target != eDiscardPile) {return};
-  
-  if(rodada > 1){
-    while(eDiscardPile.firstChild) {
-      eDiscardPile.removeChild(eDiscardPile.firstChild);
-  }
-  }
-  
-  let descarte = document.getElementById("descarte");
-  descarte.appendChild(arrastado);
 
-  arrastado.add
-  
-  playCard({efeito: arrastado.dataset.efeito, symbol: arrastado.dataset.symbol});
-  
+  // if (rodada > 1) {
+  //   while (eDiscardPile.firstChild) {
+  //     eDiscardPile.removeChild(eDiscardPile.firstChild);
+  //   }
+  // }
+
+  // let descarte = document.querySelector("#discard");
+  // descarte.appendChild(arrastado);
+
+  // arrastado.add;
+
+  playCard({
+    efeito: arrastado.dataset.efeito,
+    symbol: arrastado.dataset.symbol,
+  });
+
   updateCards();
   moveSnake();
   updateSnake();
+  updatePlayed();
+  updateDiscard();
   rodada++;
   arrastado = null;
+}
+
+function reshuffleCards() {
+  if (getCards().length > 0) return;
+  drawHand();
+}
+
+function createCard(card, id) {
+  const eCard = document.createElement("div");
+  eCard.classList.add("card");
+  eCard.setAttribute("draggable", true);
+  eCard.addEventListener("dragstart", dragCard);
+  eCard.dataset.efeito = card.efeito;
+  eCard.title = card.efeito;
+  eCard.dataset.symbol = card.symbol;
+  eCard.dataset.id = id;
+
+  const eSymbol = document.createElement("div");
+  eSymbol.textContent = card.symbol;
+
+  eCard.appendChild(eSymbol);
+  return eCard;
 }
